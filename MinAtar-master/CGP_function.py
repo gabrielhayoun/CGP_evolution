f2514a12a5cb66362edbd85ae7f285404b202b58
gabrielhayoun <78904699+gabrielhayoun@users.noreply.github.com>
2021-06-02 09:25:48 +0200

    play function

--- MinAtar-master/CGP_function.py
+++ MinAtar-master/CGP_function.py
@@ -1,6 +1,10 @@
 import cgp
 from minatar import Environment
 
+NUM_FRAMES = 1000
+MAX_EVALS = 5000
+
+
 def argmax(policy):
     i, max = 0, policy[0]
     for j in range(policy):
@@ -16,7 +20,6 @@ def play(individual, display=False):
     is_terminated = False
     total_reward = 0.0
     t = 0
-
     while (not is_terminated) and t < NUM_FRAMES:
         eval = individual.to_func()
         policy = eval(env.state())
@@ -28,22 +31,24 @@ def play(individual, display=False):
             env.display_state(1)
     return total_reward
 
+
 # -------------Objective Function-------------
 def objective(individual):
     individual.fitness = play(individual)
     return individual
 
+
 # -----------Parameters of population, genome, evolutionary algorithm (EA) and evolve function ------------------
 population_params = {"n_parents": 10, "mutation_rate": 0.5, "seed": 8188211}
 
 genome_params = {
-     "n_inputs": 100,
-     "n_outputs": 3,
-     "n_columns": 10,
-     "n_rows": 1,
-     "levels_back": 10,
-     "primitives": (cgp.Add, cgp.Sub, cgp.Mul, cgp.Div, cgp.ConstantFloat),
-     }
+    "n_inputs": 100,
+    "n_outputs": 3,
+    "n_columns": 10,
+    "n_rows": 1,
+    "levels_back": 10,
+    "primitives": (cgp.Add, cgp.Sub, cgp.Mul, cgp.Div, cgp.ConstantFloat),
+}
 
 ea_params = {"n_offsprings": 10, "tournament_size": 2, "n_processes": 2}
 
@@ -56,6 +61,8 @@ ea = cgp.ea.MuPlusLambda(**ea_params)
 # ---------Record information------------
 history = {}
 history["fitness_parents"] = []
+
+
 def recording_callback(pop):
     history["fitness_parents"].append(pop.fitness_parents())
 
