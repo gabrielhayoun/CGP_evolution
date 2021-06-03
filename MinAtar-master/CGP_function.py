595ff30e8958b0d5265b60a727149d4035191798
Aymeric <Khopfor>
2021-06-03 10:39:13 +0200

    convert_state_func

--- MinAtar-master/CGP_function.py
+++ MinAtar-master/CGP_function.py
@@ -13,17 +13,32 @@ def argmax(policy):
             i = j
     return j
 
+def convertState (state, nbObjects): #
+    newState = [] 
+    for i in range (len(state)):
+        newState.append([])
+        for j in range (len(state[0])):
+            c=nbObjects
+            while c>0 and not(state[i][j][c-1]) :
+                c-=1
+            pixelValue = c/nbObjects
+            newState[-1].append(pixelValue)
+    return newState
+
+
 
 def play(individual, display=False):
-    env = Environment("breakout", sticky_action_prob=0.0, random_seed=0)
+    env = Environment("freeway", sticky_action_prob=0.0, random_seed=0)
     env.reset()
+    nbObjects = len(env.state()[0][0])
     is_terminated = False
     total_reward = 0.0
     t = 0
 
     while (not is_terminated) and t < NUM_FRAMES:
         eval = individual.to_func()
-        policy = eval(env.state())
+        # if t==0 : print(convertState(env.state(),nbObjects))
+        policy = eval(convertState(env.state(),nbObjects))
         action = argmax(policy)
         reward, is_terminated = env.act(action)
         total_reward += reward
@@ -40,7 +55,7 @@ def objective(individual):
 
 
 # -----------Parameters of population, genome, evolutionary algorithm (EA) and evolve function ------------------
-population_params = {"n_parents": 10, "mutation_rate": 0.5, "seed": 8188211}
+population_params = {"n_parents": 10,  "seed": 8188211}
 
 genome_params = {
     "n_inputs": 100,
@@ -51,7 +66,7 @@ genome_params = {
     "primitives": (cgp.Add, cgp.Sub, cgp.Mul, cgp.Div, cgp.ConstantFloat),
 }
 
-ea_params = {"n_offsprings": 10, "tournament_size": 2, "n_processes": 3}
+ea_params = {"n_offsprings": 10, "tournament_size": 2, "mutation_rate": 0.5,"n_processes": 3}
 
 evolve_params = {"max_generations": 1000, "min_fitness": 0.0}
 
